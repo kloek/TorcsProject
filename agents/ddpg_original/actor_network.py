2457c2f2c99819e9ccde51ef6c56df82f1478efe
Anton Kloek <anton.kloek@gmail.com>
2017-07-10 14:27:15 +0200

    added Batch Notmalization

--- agents/ddpg_original/actor_network.py
+++ agents/ddpg_original/actor_network.py
@@ -20,21 +20,17 @@ class Actor(object):
         self.state_dim = state_dim
         self.action_dim = action_dim
 
-
+        ### Initialize target network μ′ with weights θμ′ ← θμ
         # create actor network
-
         self.state_input,\
         self.action_output,\
-        self.net,\
-        self.is_training = self.create_network(state_dim, action_dim)
+        self.net = self.create_network(state_dim, action_dim)
 
-        ### Initialize target network μ′ with weights θμ′ ← θμ
         # create target actor network
         self.target_state_input,\
         self.target_action_output,\
         self.target_update,\
-        self.target_net,\
-        self.target_is_training = self.create_target_network(state_dim, action_dim, self.net)
+        self.target_net = self.create_target_network(state_dim, action_dim, self.net)
 
         # define training rules
         self.create_training_method()
@@ -57,7 +53,6 @@ class Actor(object):
 
         # input
         state_input = tf.placeholder("float", [None, state_dim])
-        is_training = tf.placeholder(tf.bool) # place holder for BN
 
         W1_shape = [state_dim, layer1_size]
         W1 = tf.Variable(tf.random_uniform(W1_shape, -1 / math.sqrt(state_dim), 1 / math.sqrt(state_dim)))
@@ -81,60 +76,46 @@ class Actor(object):
         W_brake = tf.Variable(tf.random_uniform([layer2_size, 1], -1e-4, 1e-4))
         b_brake = tf.Variable(tf.random_uniform([1], -1e-4, 1e-4))
 
-        layer0_bn = self.batch_norm_layer(state_input, training_phase=is_training, scope_bn='batch_norm_0',
-                                          activation=tf.identity)
-        layer1 = tf.matmul(layer0_bn, W1) + b1
-        layer1_bn = self.batch_norm_layer(layer1, training_phase=is_training, scope_bn='batch_norm_1',
-                                          activation=tf.nn.relu)
-        layer2 = tf.matmul(layer1_bn, W2) + b2
-        layer2_bn = self.batch_norm_layer(layer2, training_phase=is_training, scope_bn='batch_norm_2',
-                                          activation=tf.nn.relu)
+        layer1 = tf.nn.relu(tf.matmul(state_input, W1) + b1)
+        layer2 = tf.nn.relu(tf.matmul(layer1, W2) + b2)
 
         #print("steer= " + str(tf.matmul(layer2, W_steer)) + " b_steer= " + str(b_steer))
-        steer = tf.tanh(tf.matmul(layer2_bn, W_steer) + b_steer)
-        accel = tf.sigmoid(tf.matmul(layer2_bn, W_accel) + b_accel)
-        brake = tf.sigmoid(tf.matmul(layer2_bn, W_brake) + b_brake)
+        steer = tf.tanh(tf.matmul(layer2, W_steer) + b_steer)
+        accel = tf.sigmoid(tf.matmul(layer2, W_accel) + b_accel)
+        brake = tf.sigmoid(tf.matmul(layer2, W_brake) + b_brake)
 
         # action_output = tf.concat(1, [steer, accel, brake])
         action_output = tf.concat([steer, accel, brake], 1)
 
-        return state_input, action_output, [W1, b1, W2, b2, W_steer, b_steer, W_accel, b_accel, W_brake, b_brake], is_training
+        return state_input, action_output, [W1, b1, W2, b2, W_steer, b_steer, W_accel, b_accel, W_brake, b_brake]
 
     # TODO, could original "create_network" be used for both?
     def create_target_network(self, state_dim, action_dim, net):
+
         state_input = tf.placeholder("float", [None, state_dim])
-        is_training = tf.placeholder(tf.bool)
         ema = tf.train.ExponentialMovingAverage(decay=1 - TAU)
         target_update = ema.apply(net)
         target_net = [ema.average(x) for x in net]
 
-        layer0_bn = self.batch_norm_layer(state_input, training_phase=is_training, scope_bn='target_batch_norm_0',
-                                          activation=tf.identity)
+        layer1 = tf.nn.relu(tf.matmul(state_input, target_net[0]) + target_net[1])
+        layer2 = tf.nn.relu(tf.matmul(layer1, target_net[2]) + target_net[3])
 
-        layer1 = tf.matmul(layer0_bn, target_net[0]) + target_net[1]
-        layer1_bn = self.batch_norm_layer(layer1, training_phase=is_training, scope_bn='target_batch_norm_1',
-                                          activation=tf.nn.relu)
-        layer2 = tf.matmul(layer1_bn, target_net[2]) + target_net[3]
-        layer2_bn = self.batch_norm_layer(layer2, training_phase=is_training, scope_bn='target_batch_norm_2',
-                                          activation=tf.nn.relu)
+        steer = tf.tanh(tf.matmul(layer2, target_net[4]) + target_net[5])
+        accel = tf.sigmoid(tf.matmul(layer2, target_net[6]) + target_net[7])
+        brake = tf.sigmoid(tf.matmul(layer2, target_net[8]) + target_net[9])
 
-        steer = tf.tanh(tf.matmul(layer2_bn, target_net[4]) + target_net[5])
-        accel = tf.sigmoid(tf.matmul(layer2_bn, target_net[6]) + target_net[7])
-        brake = tf.sigmoid(tf.matmul(layer2_bn, target_net[8]) + target_net[9])
-
-        #action_output = tf.concat(1, [steer, accel, brake])
+        # action_output = tf.concat(1, [steer, accel, brake])
         action_output = tf.concat([steer, accel, brake], 1)
-
-        return state_input, action_output, target_update, target_net, is_training
+        return state_input, action_output, target_update, target_net
 
     def create_training_method(self):
         self.q_gradient_input = tf.placeholder("float", [None, self.action_dim])
         self.parameters_gradients = tf.gradients(self.action_output, self.net, -self.q_gradient_input)
-
-        for i, grad in enumerate(self.parameters_gradients):
-            if grad is not None:
-                self.parameters_gradients[i] = tf.clip_by_value(grad, -2.0,2.0)
-
+        # '''
+        # for i, grad in enumerate(self.parameters_gradients):
+        #    if grad is not None:
+        #        self.parameters_gradients[i] = tf.clip_by_value(grad, -2.0,2.0)
+        # '''
         self.optimizer = tf.train.AdamOptimizer(LEARNING_RATE).apply_gradients(zip(self.parameters_gradients, self.net))
 
 
@@ -145,35 +126,24 @@ class Actor(object):
     def train(self, q_gradient_batch, state_batch):
         self.session.run(self.optimizer, feed_dict={
             self.q_gradient_input: q_gradient_batch,
-            self.state_input: state_batch,
-            self.is_training: True
+            self.state_input: state_batch
         })
 
     def actions(self, state_batch):
         return self.session.run(self.action_output, feed_dict={
-            self.state_input: state_batch,
-            self.is_training: True
+            self.state_input: state_batch
         })
 
     def action(self, state):
         # print(str(state))
         return self.session.run(self.action_output, feed_dict={
-            self.state_input: [state],
-            self.is_training: False
+            self.state_input: [state]
         })[0]
 
     def target_actions(self, state_batch):
         return self.session.run(self.target_action_output, feed_dict={
-            self.target_state_input: state_batch,
-            self.target_is_training: True
+            self.target_state_input: state_batch
         })
-
-    def batch_norm_layer(self,x,training_phase,scope_bn,activation=None):
-        return tf.cond(training_phase,
-        lambda: tf.contrib.layers.batch_norm(x, activation_fn=activation, center=True, scale=True,
-        updates_collections=None,is_training=True, reuse=None,scope=scope_bn,decay=0.9, epsilon=1e-5),
-        lambda: tf.contrib.layers.batch_norm(x, activation_fn =activation, center=True, scale=True,
-        updates_collections=None,is_training=False, reuse=True,scope=scope_bn,decay=0.9, epsilon=1e-5))
 
     def print_settings(self, settings_file):
         settings_text = ["\n\n==== from actor network ====" + "\n",
@@ -184,7 +154,6 @@ class Actor(object):
                          "BATH_SIZE = " + str(BATH_SIZE) + "\n"]
         for line in settings_text:
             settings_file.write(line)  # print settings to file
-
 
     """def variable(self,shape,f):
         return tf.Variable(tf.random_uniform(shape,-1/math.sqrt(f),1/math.sqrt(f)))"""
