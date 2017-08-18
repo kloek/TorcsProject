69c6a2c0a2b9a9f48dd17e8b4cf62a2fbfb1dc20
Anton Kloek <anton.kloek@gmail.com>
2017-08-18 13:18:52 +0200

    unfinnished work

--- agents/ddpg_original/actor_network.py
+++ agents/ddpg_original/actor_network.py
@@ -19,13 +19,13 @@ class Actor:
         self.action_dim = action_dim
 
         # create actor network
-        self.state_input, self.action_output, self.net = self.create_network(state_dim, action_dim)
+        self.state_input, self.action_output, self.net, self.img_input, self.conv_net = self.create_network(state_dim, action_dim)
         print("Actor network = " + str(self.net))
 
         # create target actor network
         self.target_state_input, self.target_action_output, self.target_update, self.target_net = self.create_target_network(
-            state_dim, action_dim, self.net)
-        print("Actor target_network = " + str(self.target_net))
+            state_dim, action_dim, self.net, self.conv_net)
+        #print("Actor target_network = " + str(self.target_net))
 
         # define training rules
         self.create_training_method()
@@ -38,21 +38,57 @@ class Actor:
     def create_training_method(self):
         self.q_gradient_input = tf.placeholder("float", [None, self.action_dim])
         self.parameters_gradients = tf.gradients(self.action_output, self.net, -self.q_gradient_input)
-        '''        
-        for i, grad in enumerate(self.parameters_gradients):
-            if grad is not None:
-                self.parameters_gradients[i] = tf.clip_by_value(grad, -2.0,2.0)
-        '''
         self.optimizer = tf.train.AdamOptimizer(LEARNING_RATE).apply_gradients(zip(self.parameters_gradients, self.net))
 
     def create_network(self, state_dim, action_dim):
+        print(" === Create Network (Actor) === ")
+
+        #fully connected layers
         layer1_size = LAYER1_SIZE
         layer2_size = LAYER2_SIZE
 
+        # input for image
+        img_input = tf.placeholder(dtype="float", shape=[None, 64, 64, 3], name="image_input")
+        # Input for sensors, not part of conv. net
         state_input = tf.placeholder("float", [None, state_dim], name="state_input")
 
+        #conv layer 1
+        conv1 = tf.layers.conv2d(
+            inputs=img_input,
+            filters=68,
+            kernel_size=[5,5],
+            padding="same",
+            activation=tf.nn.relu,
+            name="conv1")
+
+        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2,2], strides=2, name="pool1")
+
+        # conv layer 2
+        conv2 = tf.layers.conv2d(
+            inputs=pool1,
+            filters=136,  # assumed double 68, since guide did double 32
+            kernel_size=[5, 5],
+            padding="same",
+            activation=tf.nn.relu,
+            name="conv2")
+
+        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2, name="pool2")
+
+        # Flatten last pool
+        flat_size = 16*16*136
+        pool2_flat = tf.reshape(pool2, [-1,flat_size])
+
+        print("conv1 = " + str(conv1))
+        print("pool1 = " + str(pool1))
+        print("conv2 = " + str(conv2))
+        print("pool2 = " + str(pool2))
+
+
+        ## Fully Connected layers!
+
         #W1 = self.variable([state_dim, layer1_size], state_dim)
-        W1 = tf.Variable(tf.random_uniform([state_dim, layer1_size], -1 / math.sqrt(state_dim), 1 / math.sqrt(state_dim)), name="W1")
+        W1_sens = tf.Variable(tf.random_uniform([state_dim, layer1_size], -1 / math.sqrt(state_dim), 1 / math.sqrt(state_dim)), name="W1_sens")
+        W1_vision = tf.Variable(tf.random_uniform([flat_size, layer1_size], -1 / math.sqrt(flat_size), 1 / math.sqrt(flat_size)), name="W1_vision")
         #b1 = self.variable([layer1_size], state_dim)
         b1 = tf.Variable(tf.random_uniform([layer1_size], -1 / math.sqrt(state_dim), 1 / math.sqrt(state_dim)), name="b1")
 
@@ -73,7 +109,7 @@ class Actor:
         W_brake = tf.Variable(tf.random_uniform([layer2_size, 1], -1e-4, 1e-4), name="W_brake")
         b_brake = tf.Variable(tf.random_uniform([1], -1e-4, 1e-4), name="b_brake")
 
-        layer1 = tf.nn.relu(tf.matmul(state_input, W1) + b1, name="layer1")
+        layer1 = tf.nn.relu(tf.matmul(state_input, W1_sens) + tf.matmul(pool2_flat, W1_vision) + b1, name="layer1")
         layer2 = tf.nn.relu(tf.matmul(layer1, W2) + b2, name="layer2")
 
         steer = tf.tanh(tf.matmul(layer2, W_steer) + b_steer, name="steer")
@@ -82,13 +118,24 @@ class Actor:
 
         action_output = tf.concat([steer, accel, brake], 1, name="action_output")
 
-        return state_input, action_output, [W1, b1, W2, b2, W_steer, b_steer, W_accel, b_accel, W_brake, b_brake]
+        return state_input, action_output, [W1_sens, b1, W2, b2, W_steer, b_steer, W_accel, b_accel, W_brake, b_brake], img_input, [conv1, pool1, conv2, pool2, pool2_flat]
 
-    def create_target_network(self, state_dim, action_dim, net):
+    def create_target_network(self, state_dim, action_dim, net, conv_net):
+        print(" === Create Target Network (Actor) === ")
+
         state_input = tf.placeholder("float", [None, state_dim])
         ema = tf.train.ExponentialMovingAverage(decay=1 - TAU)
         target_update = ema.apply(net)
         target_net = [ema.average(x) for x in net]
+
+        img_input = tf.placeholder(dtype="float", shape=[None, 64, 64, 3], name="image_input")
+        ema_conv = tf.train.ExponentialMovingAverage(decay=1 - TAU)
+        target_update_conv = ema.apply(conv_net)
+        target_conv_net = [ema.average(x) for x in conv_net]
+
+        print("net = " + str(net))
+        print("target_net = " + str(target_net))
+        print("conv_net = " + str(conv_net))
 
         layer1 = tf.nn.relu(tf.matmul(state_input, target_net[0]) + target_net[1])
         layer2 = tf.nn.relu(tf.matmul(layer1, target_net[2]) + target_net[3])
