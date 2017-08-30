3797307fea6e693c464c4c1156b5174475c903ab
Anton Kloek <anton.kloek@gmail.com>
2017-08-30 22:28:30 +0200

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,33 +3,25 @@ echo "Launch TORCS simulation"
 
 # get docker image
 echo "Pull the docker image..."
-sudo docker pull kloek/torcsproject
+sudo docker pull kloek/torcsproject:test
 echo "...done"
 echo ""
-
-# TODO select config file before running?
 
 # start the virtual gui
 echo "Run the docker image..."
-id="$(sudo docker run -td kloek/torcsproject Xvfb :1 -screen 0 800x600x16)"
-echo "...done, running with id = $id"
-echo ""
-
-# update the repo
-echo "Update the gym_torcs and agents repo..."
-sudo docker exec -t $id git pull origin master
-sudo docker exec -t $id sh -c 'cd /TorcsProject; git pull origin master'
+sudo docker run -td kloek/torcsproject Xvfb :1 -screen 0 800x600x16
 echo "...done"
 echo ""
 
-echo "move agents to gym_torcs"
-sudo docker exec -t $id sh -c 'cd /TorcsProject; ./install.sh'
-echo "... done"
+# update the repo
+echo "Update the repo..."
+sudo docker exec -t $(sudo docker ps -lq) git pull origin master
+echo "...done"
 echo ""
 
 # create the fake display to run torcs
 echo "Open fake display..."
-sudo docker exec -td $id x11vnc -forever -create -display :1.0
+sudo docker exec -td $(sudo docker ps -lq) x11vnc -forever -create -display :1.0
 echo "...done"
 echo ""
 
@@ -37,13 +29,4 @@ echo ""
 echo "Starting the simulation !"
 echo ""
 #sudo docker exec -t -e "DISPLAY=:1.0" $(sudo docker ps -lq) bash -c "python run_ddpg.py"
-sudo docker exec -t -e "DISPLAY=:1.0" $id bash -c "python3 run_ddpg.py"
-
-echo "Move results out from docker"
-sudo docker cp $id:~/Anton/runs/* ~/Anton/runs
-echo "...done"
-
-
-#echo "Stop and remove dockers"
-#sudo docker stop $id
-#sudo docker rm $id
+sudo docker exec -t -e "DISPLAY=:1.0" $(sudo docker ps -lq) bash -c "python3 run_ddpg.py"
